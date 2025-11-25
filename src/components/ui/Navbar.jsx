513355774c7f19408759b3f3fe595514a5c99d4b
David Bilan <bilandavid07@gmail.com>
2025-11-25 16:34:54 +0100

    scalable sizes for all navbar items

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -8,7 +8,7 @@ import { useTranslation } from 'react-i18next';
 export default function Navbar({ activeIndex, onNavigate }) {
     const labels = ["home_nav_link", "paintings_nav_link", "workshops_nav_link", "about_nav_link"];
 
-    const {t, i18n} = useTranslation();
+    const { t, i18n } = useTranslation();
 
     const switchLanguage = (lng) => i18n.changeLanguage(lng);
 
@@ -18,17 +18,21 @@ export default function Navbar({ activeIndex, onNavigate }) {
             {/* Top: language */}
             <div className="flex justify-end">
 
-                <div className={` ${i18n.language === 'ua' ? "font-semibold" : "font-normal"} text-xl cursor-pointer "`} onClick={ () =>switchLanguage('ua')}>ua</div>
+                <div className={` ${i18n.language === 'ua' ? "font-semibold" : "font-normal"} md:text-sm xl:text-xl cursor-pointer "`} onClick={() => switchLanguage('ua')}>
+                    ua
+                </div>
 
-                <div className="text-xl px-3">|</div>
+                <div className="md:text-sm xl:text-xl md:mx-1 xl:mx-2 2xl:mx-3">|</div>
 
 
-                <div className={` ${i18n.language === 'en' ? "font-semibold" : "font-normal"} text-xl cursor-pointer "`} onClick={ () =>switchLanguage('en')}>en</div>
+                <div className={` ${i18n.language === 'en' ? "font-semibold" : "font-normal"} md:text-sm xl:text-xl cursor-pointer "`} onClick={() => switchLanguage('en')}>
+                    en
+                </div>
 
             </div>
 
             {/* Middle: section links */}
-            <div className="flex flex-col items-end space-y-6 text-xl">
+            <div className="flex flex-col items-end space-y-6  md:text-sm xl:text-base 2xl:text-xl">
                 {labels.map((label, i) => (
                     <button
                         key={i}
@@ -46,17 +50,17 @@ export default function Navbar({ activeIndex, onNavigate }) {
                 <div className="flex flex-col  space-y-4 transition-all">
                     <a href="mailto:ira@example.com" target="_blank" className=" flex justify-center">️
 
-                        <EmailIcon className="w-7 fill-black icon-hover" />
+                        <EmailIcon className="md: w-5 xl:w-6 2xl:w-8 fill-black icon-hover" />
 
                     </a>
-                    <a href="https://www.threads.com/@iriina.simonyan?igshid=NTc4MTIwNjQ2YQ==" target="_blank"  className=" flex justify-center">
+                    <a href="https://www.threads.com/@iriina.simonyan?igshid=NTc4MTIwNjQ2YQ==" target="_blank" className=" flex justify-center">
 
-                        <ThreadsIcon className="w-8 fill-black icon-hover" />
+                        <ThreadsIcon className=" md: w-6 xl:w-7 2xl:w-9 fill-black icon-hover" />
 
                     </a>
-                    <a href="https://www.instagram.com/iriina.simonyan?igsh=MTNtbnFvMmZldzlheA%3D%3D&utm_source=qr" target="_blank"  className="flex justify-center">
+                    <a href="https://www.instagram.com/iriina.simonyan?igsh=MTNtbnFvMmZldzlheA%3D%3D&utm_source=qr" target="_blank" className="flex justify-center">
 
-                        <InstagramIcon className="w-10 fill-black icon-hover" />
+                        <InstagramIcon className="md: w-7 xl:w-8 2xl:w-10 fill-black icon-hover" />
 
                     </a>
 
