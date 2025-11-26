e7ed2b50ec6382cca035a86728d5713396af159f
David Bilan <bilandavid07@gmail.com>
2025-11-26 14:59:43 +0100

    Added gallery to paintings section

--- src/sections/Paintings.jsx
+++ src/sections/Paintings.jsx
@@ -1,7 +1,16 @@
-export default function Paintings() {
+import PaintingCarousel from "@/components/ui/PaintingsCarousel";
+import { useTranslation } from 'react-i18next';
+
+export default function Paintings({ onPhotoClick }) {
+  const { t } = useTranslation();
+
   return (
-    <section className="section h-full bg-blue-400 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">Paintings</h1>
+    <section className="section h-full bg-gray-100 flex flex-col items-center justify-center">
+      {/* Heading */}
+      <h2 className="text-3xl font-light mb-6">{t("gallery")}</h2>
+
+      {/* Carousel */}
+      <PaintingCarousel onPhotoClick={onPhotoClick} />
     </section>
   );
 }
