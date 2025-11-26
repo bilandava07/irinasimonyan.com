8232bf0f8a65adc52b2893c0270dd454ed0027fc
David Bilan <bilandavid07@gmail.com>
2025-11-26 14:59:17 +0100

    move logo to mainlayout

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -4,17 +4,12 @@ export default function Home() {
   return (
     <div className="relative h-[100vh] w-full flex justify-end overflow-hidden">
 
-      {/* LOGO — POSITIONED ABSOLUTELY & INDEPENDENTLY */}
-      <div className="absolute top-6 left-6 z-50">
-        <h2 className="text-3xl md:text-3xl font-light whitespace-nowrap">
-          Irina Simonyan
-        </h2>
-      </div>
+
 
       <div className=" max-w-[80vw] h-full flex items-center ml-auto relative z-10">
         {/* LEFT TEXT */}
         <div className="relative z-10">
-          <h1 className="text-6xl md:text-9xl font-bold uppercase tracking-wide">
+          <h1 className="text-6xl md:text-9xl font-bold uppercase tracking-wide ">
             <span className="block ml-0">color</span>
             <span className="block ml-20">in the</span>
             <span className="block ml-70">skin</span>
