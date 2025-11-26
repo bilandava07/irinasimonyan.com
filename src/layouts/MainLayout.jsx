69ac9e772f7cf3b1fe560d41d032a8c9c5d22b1a
David Bilan <bilandavid07@gmail.com>
2025-11-26 14:58:08 +0100

    added photo overlay for clicked paintings

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,10 +4,15 @@ import Home from "@/sections/Home";
 import Paintings from "@/sections/Paintings";
 import Workshops from "@/sections/Workshops";
 import About from "@/sections/About";
+import PhotoOverlay from "../components/ui/PhotoOverlay";
 
 const sections = ["home", "paintings", "workshops", "about"];
 
 export default function MainLayout() {
+
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+
   const [activeSection, setActiveSection] = useState("home");
   const containerRef = useRef(null);
 
@@ -31,24 +36,51 @@ export default function MainLayout() {
     return () => observer.disconnect();
   }, []);
 
+
+  function handlePhotoClick(photo) {
+    setSelectedPhoto(photo);
+  }
+
+  function handleCloseOverlay() {
+    setSelectedPhoto(null);
+  }
+
   return (
-    <div className="flex w-full ">
-      
-      {/* Scrollable Content */}
-      <div
-      
-        className="flex-1 h-full overflow-y-hidden scroll-smooth"
-      >
-        <section id="home" className="h-screen"><Home /></section>
-        <section id="paintings" className="h-screen"><Paintings /></section>
-        <section id="workshops" className="h-screen"><Workshops /></section>
-        <section id="about" className="h-screen"><About /></section>
+    <>
+
+
+
+      <div className="flex w-full ">
+        {/* LOGO — POSITIONED ABSOLUTELY & INDEPENDENTLY */}
+        <div className="fixed top-6 left-6 z-50">
+          <h2 className="text-3xl md:text-3xl font-light whitespace-nowrap">
+            Irina Simonyan
+          </h2>
+        </div>
+
+        {/* Scrollable Content */}
+        <div
+
+          className="flex-1 h-full overflow-y-hidden scroll-smooth"
+        >
+          <section id="home" className="h-screen"><Home /></section>
+          <section id="paintings" className="h-screen"><Paintings onPhotoClick={handlePhotoClick} /></section>
+          <section id="workshops" className="h-screen"><Workshops /></section>
+          <section id="about" className="h-screen"><About /></section>
+        </div>
+
+        {/* Navbar */}
+        <div className="w-[12%] min-w-[120px] border-l h-full ">
+          <Navbar activeSection={activeSection} />
+        </div>
       </div>
 
-      {/* Navbar */}
-      <div className="w-[12%] min-w-[120px] border-l h-full ">
-        <Navbar activeSection={activeSection} />
-      </div>
-    </div>
+
+      <PhotoOverlay selectedPhoto={selectedPhoto} handleCloseOverlay={handleCloseOverlay} />
+
+    </>
+
+
+
   );
 }
