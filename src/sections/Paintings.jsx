d2ca5d17706775e6cb7d70c8fb4270074a9f9225
David Bilan <bilandavid07@gmail.com>
2025-11-27 20:02:29 +0100

    Paintings section done for laptops and desktops

--- src/sections/Paintings.jsx
+++ src/sections/Paintings.jsx
@@ -4,10 +4,11 @@ export default function Paintings({ onPhotoClick }) {
 
 
   return (
-    <section className="section h-full bg-gray-100 flex flex-col items-center justify-center ">
+    <section className="section h-full  flex flex-col items-center justify-center ">
 
 
       <PaintingsCarousel onPhotoClick={onPhotoClick}/>
+      
     </section>
   );
 }
