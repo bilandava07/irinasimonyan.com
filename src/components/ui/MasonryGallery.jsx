953c6d9d8e8e76e81a31d3c2df142c4c2a513cf7
David Bilan <bilandavid07@gmail.com>
2025-11-27 22:44:45 +0100

    Added paintings section

---
+++ src/components/ui/MasonryGallery.jsx
@@ -0,0 +1,24 @@
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
+export default function MasonryGallery({ photos, scrollRef, onPhotoClick }) {
+  return (
+    <div
+      ref={scrollRef}
+      className="w-full h-full overflow-hidden"
+    >
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}>
+        <Masonry >
+          {photos.map((photo) => (
+            <img
+              key={photo.id}
+              src={photo.imageUrl}
+              className="w-full p-3 block object-cover cursor-pointer transition-all duration-300 hover:shadow-2xl hover:scale-101  "
+              alt={`Painting ${photo.id}`}
+              onClick={() => onPhotoClick(photo)}
+            />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+    </div>
+  );
+}
---
+++ src/sections/Portfolio.jsx
@@ -0,0 +1,121 @@
+import MasonryGallery from "../components/ui/MasonryGallery";
+import photos from "../data/photos";
+import { useRef, useState } from "react";
+import { ChevronUp, ChevronDown } from "lucide-react";
+
+import { useTranslation } from 'react-i18next';
+
+const categories = [
+  { id: "portraits", label: "cat_portraits" },
+  { id: "landscapes", label: "cat_landscapes" },
+  { id: "still", label: "cat_still" },
+  { id: "animals", label: "cat_animals" },
+];
+
+export default function Portfolio({ onPhotoClick }) {
+
+  const { t } = useTranslation();
+
+  const [selectedCategory, setSelectedCategory] = useState(categories[0].id);
+
+  // Filter photos based on selected category
+  const filteredPhotos = photos.filter(
+    (photo) => photo.category === selectedCategory
+  );
+
+
+
+  const scrollRef = useRef(null); // create the scrollRef here
+
+  const scroll = (direction) => {
+    if (!scrollRef.current) return;
+    const scrollAmount = 400;
+    scrollRef.current.scrollBy({
+      top: direction === "up" ? -scrollAmount : scrollAmount,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <section className="section h-full  flex items-center justify-center">
+
+      {/* Left sidebar */}
+
+      <div className="w-1/5 p-4 flex flex-col justify-center gap-2 h-full">
+
+        <div className="
+            flex
+            flex-col
+            justify-between 
+            h-full 
+            p-10 
+            w-[12vw] 
+            min-w-[120px]
+            bg-white"
+        >
+
+          <div className=""></div>
+
+
+
+
+
+
+          {/* Top: filters */}
+          <div className="flex flex-col space-y-6 items-start gap-2 md:text-sm xl:text-base 2xl:text-xl whitespace-nowrap">
+            {categories.map((cat) => (
+              <button
+                key={cat.id}
+                onClick={() => setSelectedCategory(cat.id)}
+                className={`flex justify-start cursor-pointer link-hover ${selectedCategory === cat.id
+                    ? "font-medium scale-105 text-black"
+                    : "font-light text-gray-700"
+                  }`}
+              >
+                {t(cat.label)}
+              </button>
+            ))}
+          </div>
+
+
+
+
+
+
+
+          <div className="flex justify-start">
+
+            <div className="flex flex-col gap-6 mb-5 ">
+              <button
+                className="chevron-container"
+                onClick={() => scroll("up")}
+              >
+                <ChevronUp className="chevron-sizing" />
+              </button>
+
+              <button
+                className="chevron-container"
+                onClick={() => scroll("down")}
+              >
+                <ChevronDown className="chevron-sizing" />
+              </button>
+
+
+
+
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+
+      <div className="flex h-full p-15 w-full">
+
+        {/* Portfolio */}
+        <MasonryGallery photos={filteredPhotos} scrollRef={scrollRef} onPhotoClick={onPhotoClick} />
+      </div>
+    </section>
+  );
+}
