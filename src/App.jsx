d5954a32d2aacc5919aedc022ef264034458a778
David Bilan <bilandavid07@gmail.com>
2025-11-27 21:00:03 +0100

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,102 @@
-import MainLayout from "@/layouts/MainLayout";
+import { useEffect, useState, useRef } from "react";
+import Navbar from "@/components/ui/Navbar";
+import Home from "@/sections/Home";
+import Paintings from "@/sections/Paintings";
+import Workshops from "@/sections/Workshops";
+import About from "@/sections/About";
+import PhotoOverlay from "@/components/ui/PhotoOverlay";
 
+const sections = ["home", "paintings", "workshops", "about"];
 function App() {
-  return <MainLayout />;
-}
+const [selectedPhoto, setSelectedPhoto] = useState(null);
 
+
+  const [activeSection, setActiveSection] = useState("home");
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      { threshold: 0.6 } // 60% visible
+    );
+
+    sections.forEach((id) => {
+      const el = document.getElementById(id);
+      if (el) observer.observe(el);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+
+  function handlePhotoClick(photo) {
+    setSelectedPhoto(photo);
+  }
+
+  function handleCloseOverlay() {
+    setSelectedPhoto(null);
+  }
+
+
+    // Disable scroll when any overlay is open
+    const isOverlayOpen = selectedPhoto !== null ;
+
+    useEffect(() => {
+        if (isOverlayOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+        return () => {
+            document.body.style.overflow = "";
+        };
+    }, [isOverlayOpen]);
+
+
+  return (
+    <>
+
+
+
+      <div className="flex w-full ">
+        {/* LOGO — POSITIONED ABSOLUTELY & INDEPENDENTLY */}
+        <div className="fixed top-6 left-6  z-20    md:w-30 xl:w-50  2xl:w-60  ">
+          <img 
+            src="/images/logo.png"
+            />
+
+
+        </div>
+
+        {/* Scrollable Content */}
+        <div
+
+          className="flex-1 h-full overflow-y-hidden scroll-smooth"
+        >
+          <section id="home" className="h-screen"><Home /></section>
+          <section id="paintings" className="h-screen"><Paintings onPhotoClick={handlePhotoClick} /></section>
+          <section id="workshops" className="h-screen"><Workshops /></section>
+          <section id="about" className="h-screen"><About /></section>
+        </div>
+
+        {/* Navbar */}
+        <div className="w-[12%] min-w-[120px] border-l h-full ">
+          <Navbar activeSection={activeSection} />
+        </div>
+      </div>
+
+
+      <PhotoOverlay selectedPhoto={selectedPhoto} handleCloseOverlay={handleCloseOverlay} />
+
+    </>
+
+
+
+  );
+}
 export default App;
