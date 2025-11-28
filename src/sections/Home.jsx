f4468b91dcec3368837f9fc0180149e8c8691bd2
David Bilan <bilandavid07@gmail.com>
2025-11-28 19:12:13 +0100

    responsiveness for the main home phrase

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,15 +9,15 @@ export default function Home() {
       <div className=" max-w-[80vw] h-full flex items-center ml-auto relative z-10">
         {/* LEFT TEXT */}
         <div className="relative z-10">
-          <h1 className="text-6xl md:text-9xl font-bold uppercase tracking-wide ">
-            <span className="block ml-0">color</span>
-            <span className="block ml-20">in the</span>
-            <span className="block ml-70">skin</span>
+          <h1 className="  md:text-4xl lg:text-6xl 2xl:text-9xl font-bold uppercase tracking-wide ">
+            <span className="block ml-0 ">color</span>
+            <span className="block ml-10 lg:ml-20 2xl:ml-20">in the</span>
+            <span className="block ml-25 lg:ml-45 2xl:ml-70">skin</span>
           </h1>
         </div>
 
         {/* RIGHT IMAGE */}
-        <div className="h-full -ml-20">
+        <div className="h-full md:-ml-5 lg:-ml-10 2xl:-ml-20">
           <img
             src="/images/portraits/test.png"
             alt="Painting"
