0f45f1ebf88fd73f601541e601ee2a91377626c3
David Bilan <bilandavid07@gmail.com>
2026-01-04 01:50:03 +0100

    The sections now use the locale text instead of hardcoded strings

--- src/sections/Workshops.jsx
+++ src/sections/Workshops.jsx
@@ -1,4 +1,8 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Workshops() {
+
+  const { t } = useTranslation();
 
   const bgImagePath = "/images/workshops_bg.webp"
   return (
@@ -8,12 +12,12 @@ export default function Workshops() {
 
         <div className="my-30 lg:mx-20 2xl:mx-40 flex-1 md:flex-[1] lg:flex-[3] 2xl:flex-[2] flex flex-col">
 
-          <h1 className="p-15  flex font-normal justify-around lg:text-3xl 2xl:text-5xl">Workshops</h1>
+          <h1 className="p-15 flex font-normal justify-around lg:text-3xl 2xl:text-5xl">{t("workshops_header")}</h1>
 
           <div className="flex flex-col gap-5 font-thin    lg:text-2xl 2xl:text-3xl leading-snug">
-            <p>I run painting workshops for both adults and children. My goal is to show that anyone can paint</p>
-            <p>I am fascinated by the interaction with people, especially the moment when fear yields to the desire to create. I always say, “Trust the process.” Seeing the surprise and joy on people’s faces when they look at their work and ask, “Did I really create this?” is one of the most rewarding parts of my journey.</p>
-            <p>During the sessions, I guide you step by step – from developing the idea to choosing colours and letting go of the fear of making mistakes. My workshops are suitable both for complete beginners and for those who simply want to spend time creatively.</p>
+            <p>{t("workshops_p1")}</p>
+            <p>{t("workshops_p2")}</p>
+            <p>{t("workshops_p3")}</p>
           </div>
 
         </div>
