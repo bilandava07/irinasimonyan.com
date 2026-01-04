403a3d20b9cf415061d1fe6d391447f304bc5161
David Bilan <bilandavid07@gmail.com>
2026-01-04 21:49:49 +0100

    Added link to the workshops inst account. Made it change color in a gradient manner and move a little to indicate that it is clickable, while sticking to the overall style of the site.

--- src/sections/Workshops.jsx
+++ src/sections/Workshops.jsx
@@ -8,16 +8,39 @@ export default function Workshops() {
   return (
     <section className="section h-full w-full overflow-hidden">
 
-      <div className="flex h-full w-full">
+      <div className="flex h-full w-full text-justify">
 
-        <div className="lg:my-30 2xl:my-50 lg:mx-20 2xl:mx-40 flex-1 md:flex-[1] lg:flex-[3] 2xl:flex-[2] flex flex-col">
 
-          <h1 className="lg:pb-9 2xl:pb-15 flex font-normal justify-around lg:text-3xl 2xl:text-5xl">{t("workshops_header")}</h1>
+        <div className="flex flex-col justify-between  lg:mx-20 2xl:mx-40 flex-1 md:flex-[1] lg:flex-[3] 2xl:flex-[2] ">
 
-          <div className="flex flex-col gap-5 font-thin    lg:text-2xl 2xl:text-3xl leading-snug">
-            <p>{t("workshops_p1")}</p>
-            <p>{t("workshops_p2")}</p>
-            <p>{t("workshops_p3")}</p>
+
+          <div className='md:mt-5 lg:mt-30 2xl:mt-50'>
+
+            <h1 className="lg:pb-9 2xl:pb-15 flex font-normal justify-around lg:text-3xl 2xl:text-5xl">{t("workshops_header")}</h1>
+
+            <div className="flex flex-col gap-5 font-thin  lg:text-xl 2xl:text-3xl leading-snug">
+              <p>{t("workshops_p1")}</p>
+              <p>{t("workshops_p2")}</p>
+              <p>{t("workshops_p3")}</p>
+            </div>
+
+            <div className='lg:text-xl 2xl:text-2xl lg:pt-10 2xl:pt-10  flex justify-end animate-float'>
+              <a
+                href="https://www.instagram.com/art.prostir.lutsk/"
+                target="_blank"
+                className="
+                font-light
+                hover:scale-110 transition-transform duration-300
+                bg-gradient-to-r from-black via-fuchsia-500 to-pink-600
+                bg-[length:300%_200%]
+                bg-clip-text text-transparent
+                animate-gradient
+              "
+              >
+                @art.prostir.lutsk
+              </a>
+            </div>
+
           </div>
 
         </div>
@@ -31,6 +54,6 @@ export default function Workshops() {
         </div>
 
       </div>
-    </section>
+    </section >
   );
 }
