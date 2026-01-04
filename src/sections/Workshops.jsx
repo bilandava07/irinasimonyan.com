e982bb8e8f8c71d18baf246573f82b2c131dfc07
David Bilan <bilandavid07@gmail.com>
2026-01-04 01:57:27 +0100

    responsiveness adjustments

--- src/sections/Workshops.jsx
+++ src/sections/Workshops.jsx
@@ -10,9 +10,9 @@ export default function Workshops() {
 
       <div className="flex h-full w-full">
 
-        <div className="my-30 lg:mx-20 2xl:mx-40 flex-1 md:flex-[1] lg:flex-[3] 2xl:flex-[2] flex flex-col">
+        <div className="lg:my-30 2xl:my-50 lg:mx-20 2xl:mx-40 flex-1 md:flex-[1] lg:flex-[3] 2xl:flex-[2] flex flex-col">
 
-          <h1 className="p-15 flex font-normal justify-around lg:text-3xl 2xl:text-5xl">{t("workshops_header")}</h1>
+          <h1 className="lg:pb-9 2xl:pb-15 flex font-normal justify-around lg:text-3xl 2xl:text-5xl">{t("workshops_header")}</h1>
 
           <div className="flex flex-col gap-5 font-thin    lg:text-2xl 2xl:text-3xl leading-snug">
             <p>{t("workshops_p1")}</p>
