366bed651a315fb6cdd12d3002d07497d845650d
David Bilan <bilandavid07@gmail.com>
2026-01-04 01:24:22 +0100

    Initial components for about and workshop sections. Workshops have additional elements later, thus the copy paste for now

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,26 +2,33 @@ export default function About() {
 
   const bgImagePath = "/images/about_me_bg.webp"
   return (
-    <section className="section h-full overflow-hidden">
+    <section className="section h-full w-full overflow-hidden">
 
-      <div className="flex h-full justify-end ">
+      <div className="flex h-full w-full">
 
-        <div className="py-30 font-thin flex flex-col">
+        <div className="my-30 lg:mx-20 2xl:mx-40 flex-1 md:flex-[1] lg:flex-[3] 2xl:flex-[2] flex flex-col">
 
-          <h1 className=" py-10 flex justify-around text-8xl"> About the artist</h1>
+          <h1 className="p-15  flex font-normal justify-around lg:text-3xl 2xl:text-5xl">Workshops</h1>
 
-          <h1 className="text-4xl px-30"> Sme text about me Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut qui quibusdam quia quasi, accusantium sint quod itaque saepe debitis repellat eaque ex. Sapiente culpa neque expedita aliquid quos obcaecati esse?</h1>
+          <div className="flex flex-col gap-5 font-thin    lg:text-2xl 2xl:text-3xl leading-snug">
+            <p>I run painting workshops for both adults and children. My goal is to show that anyone can paint</p>
+            <p>I am fascinated by the interaction with people, especially the moment when fear yields to the desire to create. I always say, “Trust the process.” Seeing the surprise and joy on people’s faces when they look at their work and ask, “Did I really create this?” is one of the most rewarding parts of my journey.</p>
+            <p>During the sessions, I guide you step by step – from developing the idea to choosing colours and letting go of the fear of making mistakes. My workshops are suitable both for complete beginners and for those who simply want to spend time creatively.</p>
+          </div>
 
         </div>
 
-        <img
-          src={bgImagePath}
-          alt="Artist giving instructions during the workshop"
-          loading="lazy"
-          className=" h-full object-containt block"
-        />
+        <div className="h-full  flex-1 md:flex-[1] lg:flex-[3] overflow-hidden">
+          <img
+            src={bgImagePath}
+            alt="Artist giving instructions during the workshop"
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
 
       </div>
     </section>
   );
 }
+
+
