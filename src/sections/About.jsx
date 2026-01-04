4afd5b02a7ff3911e15e768e775d9f8824745232
David Bilan <bilandavid07@gmail.com>
2026-01-04 21:48:09 +0100

    Minor adjustments

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -8,13 +8,13 @@ export default function About() {
   return (
     <section className="section h-full w-full overflow-hidden">
 
-      <div className="flex h-full w-full">
+      <div className="flex h-full w-full text-justify">
 
         <div className="lg:my-20 2xl:my-40 lg:mx-20 2xl:mx-40 flex-1 md:flex-[1] lg:flex-[3] 2xl:flex-[2] flex flex-col">
 
           <h1 className="lg:pb-8 2xl:pb-15  flex font-normal justify-around lg:text-3xl 2xl:text-5xl">{t("about_me_header")}</h1>
 
-          <div className="flex flex-col gap-5 font-thin    lg:text-2xl 2xl:text-3xl leading-snug">
+          <div className="flex flex-col gap-5 font-thin   lg:text-xl 2xl:text-3xl leading-snug">
             <p>{t("about_me_p1")}</p>
             <p>{t("about_me_p2")}</p>
             <p>{t("about_me_p3")}</p>
